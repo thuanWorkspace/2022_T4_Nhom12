7d034c546334aaa2ea40f0276bd2589e44c0e6b3
unknown <thuan0373535207@gmail.com>
2022-11-22 15:23:00 +0700

    khung cho hieu code

--- src/ConnectionToFileLogDatabase.java
+++ src/ConnectionToFileLogDatabase.java
@@ -1,6 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConnectionToFileLogDatabase {
 	private static ConnectionToFileLogDatabase getConnection;
@@ -11,11 +13,18 @@ public class ConnectionToFileLogDatabase {
 		String url = "jdbc:mysql://localhost:3306/controller";
 //	        String username = config.get("user_database");
 //	        String password = config.get("password_database");
-		String username = "root";
-		String password = "1234";
+//		String username = "root";
+//		String password = "1234";
+		Map<String,String> getUserPass = getUserPass();
+		String username = getUserPass.get("usernameMysql");
+		String password = getUserPass.get("passMysql");
+		
 		con = DriverManager.getConnection(url, username, password);
 	}
-
+	public static Map<String,String> getUserPass(){
+		//hieu code here!
+		return new HashMap<String, String>();
+	}
 	public static ConnectionToFileLogDatabase getInstance() throws SQLException, ClassNotFoundException {
 		if (getConnection == null)
 			getConnection = new ConnectionToFileLogDatabase();
--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,6 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		
+	}
+	public static void connect_database() {
+		Map<String,String> getUserPass = getUserPass();
+		
+		String username = getUserPass.get("usernameMysql");
+		String pass = getUserPass.get("passMysql");
+		
+	}
+	public static Map<String,String> getUserPass(){
+		//hieu code here!
+		return new HashMap<String, String>();
 	}
 }
