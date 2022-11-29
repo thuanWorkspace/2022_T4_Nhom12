4607d33a46be62f762959496e5c877bd7f15be01
thuanWorkspace <thuanworkspace@gmail.com>
2022-11-29 14:39:08 +0700

    front end access tool

--- goldPrice/src/main/java/com/example/goldprice/controller/ChartServlet.java
+++ goldPrice/src/main/java/com/example/goldprice/controller/ChartServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 @WebServlet(name = "ChartServlet", value = "/ChartServlet")
 public class ChartServlet extends HttpServlet {
@@ -16,9 +17,16 @@ public class ChartServlet extends HttpServlet {
         String hethong = request.getParameter("hethong");
         System.out.println("KV: "+khuvuc);
         System.out.println("HT: "+hethong);
-        ArrayList<Gold> list = DatawarehouseDao.listGold(khuvuc,hethong);
+        ArrayList<Gold> list = DatawarehouseDao.listGoldDefault(khuvuc,hethong);
+        list.sort((o1, o2) -> {
+            if(o1.getId_ngayhethan()<o2.getId_ngayhethan())
+                return -1;
+            else if (o2.getId_ngayhethan()>o2.getId_ngayhethan())
+                return 1;
+            return 0;
+        });
         request.setAttribute("list",list);
-        request.getRequestDispatcher("chart.js").forward(request,response);
+        request.getRequestDispatcher("chart.jsp").forward(request,response);
     }
 
     @Override
--- goldPrice/src/main/java/com/example/goldprice/controller/testMain.java
+++ goldPrice/src/main/java/com/example/goldprice/controller/testMain.java
@@ -5,18 +5,18 @@ import java.util.Comparator;
 
 public class testMain {
     public static void main(String[] args) {
-        Gold g1 = new Gold(1,1,10,
-                20,6000,60001,
-                "a","a","date1","date2");
-        Gold g2 = new Gold(1,1,10,
-                20,6001,60002,
-                "a","a","date1","date2");
-        Gold g3 = new Gold(1,1,10,
-                20,6002,60003,
-                "a","a","date1","date2");
-        Gold g4 = new Gold(1,1,10,
-                20,6003,60004,
-                "a","a","date1","date2");
+        Gold g1 = new Gold(1,1,60,
+                70,6000,60001,
+                "a","a","26-11-2022","27-11-2022");
+        Gold g2 = new Gold(1,1,65,
+                67,6001,60002,
+                "a","a","27-11-2022","28-11-2022");
+        Gold g3 = new Gold(1,1,66,
+                68,6002,60003,
+                "a","a","28-11-2022","29-11-2022");
+        Gold g4 = new Gold(1,1,50,
+                64,6003,60004,
+                "a","a","29-11-2022","30-11-2022");
         ArrayList<Gold> list = new ArrayList<Gold>();
 
         list.add(g2);
@@ -45,6 +45,9 @@ public class testMain {
         ) {
             System.out.println(g);
         }
-
+        for (Gold g:list
+        ) {
+            System.out.println(g.getNgaycapnhat());
+        }
     }
 }
